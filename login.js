f0469887675115a35e0e63ad011e55bca18c88f1
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2022-10-13 13:17:43 +0530

    upadted login page

--- login.js
+++ login.js
@@ -1,28 +1,64 @@
+// let data = JSON.parse(localStorage.getItem("inputData"));
+// document.querySelector('form').addEventListener('submit', loginFxn)
+// let loginData = [];
+// function loginFxn(e) {
+//     e.preventDefault();
+
+//     var obj = {};
+
+//     let email = document.querySelector('#email').value;
+//     let password = document.querySelector('#password').value;
+//     // let username = document.querySelector('#name').value
+
+
+//     obj.email = email;
+//     obj.password = password;
+//     // obj.username = username;
+
+//     let present = data.filter(function (e) {
+//         return email == e.email && password == e.password
+//     })
+
+//     loginData.push(obj);
+//     console.log(data);
+
+//     localStorage.setItem('loginData', JSON.stringify(loginData))
+
+//     if (present.length == 0) {
+//         alert("wrong details entered");
+//     }
+//     else {
+//         alert("Login Sucessfully");
+
+//         window.location.href = 'Navbar.html';
+//     }
+
+
+// }
+
+
+// new js*****************************************
 let data = JSON.parse(localStorage.getItem("inputData"));
 document.querySelector('form').addEventListener('submit', loginFxn)
 let loginData = [];
 function loginFxn(e) {
     e.preventDefault();
 
-    var obj = {};
+
 
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
     // let username = document.querySelector('#name').value
-
-
-    obj.email = email;
-    obj.password = password;
-    // obj.username = username;
-
     let present = data.filter(function (e) {
-        return email == e.email && password == e.password
+        if (email == e.email && password == e.password) {
+            return e;
+        }
     })
 
-    loginData.push(obj);
-    console.log(data);
 
-    localStorage.setItem('loginData', JSON.stringify(loginData))
+    console.log(present);
+
+    localStorage.setItem("loginData", JSON.stringify(present))
 
     if (present.length == 0) {
         alert("wrong details entered");
